7b7f0f93cb0960cba09036fde0bd18ce5f5716de
krisnawid <krisna.widi.kw@gmail.com>
2020-10-29 19:29:30 +0700

    Initial commit

---
+++ app/src/main/java/com/tumbasgo/customer/fragment/SdkConfig.java
@@ -0,0 +1,6 @@
+package com.tumbasgo.customer.fragment;
+
+public class SdkConfig {
+    public static final String MERCHANT_BASE_CHECKOUT_URL = "http://192.168.137.1:80/admin-aplikasi/api/server-test.php/";
+    public static final String MERCHANT_CLIENT_KEY = "SB-Mid-client-Q2WKmlZWmoYxVJgZ";
+}
